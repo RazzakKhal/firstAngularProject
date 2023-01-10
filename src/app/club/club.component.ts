16af37811c42a80c69c8dced942ef183a284f79a
Razzakkhal <[khalfallah.razzak@gmail.com](mailto:khalfallah.razzak@gmail.com)>
2023-01-10 23:39:56 +0100

    ajout de ma variable souscription afin de pouvoir unsubscribe dans destroy

--- src/app/club/club.component.ts
+++ src/app/club/club.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Sportif } from '../models/sportif';
 import { ClubService } from '../services/club.service';
 
@@ -7,17 +8,22 @@ import { ClubService } from '../services/club.service';
   templateUrl: './club.component.html',
   styleUrls: ['./club.component.css']
 })
-export class ClubComponent {
+export class ClubComponent implements OnDestroy {
 
 sportifs : Sportif [] | undefined;
+subscrition : Subscription;
 
   constructor(clubService : ClubService){
-clubService.getSportifs().subscribe({
+this.subscrition = clubService.getSportifs().subscribe({
   next : (donnees) => {this.sportifs = donnees},
   error : () => {console.log('il y a une erreur')},
   complete : () => {console.log('requete terminée')}
 
 });
+  }
+
+  ngOnDestroy(): void {
+    this.subscrition.unsubscribe();
   }
 
 }
